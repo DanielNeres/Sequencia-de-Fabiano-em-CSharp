86327552eb5d89128b6aa38e3dc7ce76347f9ef5
DanielNeres <danielnerres@gmail.com>
2023-12-10 09:57:00 -0300

    Sequencia de Fabiano Ao Contrario V.2

--- Sequencia de Fabiano Ao Contrario.cs
+++ Sequencia de Fabiano Ao Contrario.cs
@@ -1,7 +1,3 @@
-#include<stdio.h>
-#include<math.h> // chama a biblioteca "math.h" para efetuar cÃ¡lculos com raizes e potencia
-
-int main(){
 	int numeroimprecao, i; 
 // incia um for que percorre todas a sequencia de fibonacci, comeÃ§ando de i atÃ© i=0
 	for (i = 10; i >= 0; i--) {
@@ -9,5 +5,3 @@ int main(){
 		numeroimprecao = (pow((1 + sqrt(5)), i) - (pow((1 - sqrt(5)), i))) /(pow(2, i) * sqrt(5));
         printf("%d\n", numeroimprecao);
     }
-	return 0;
-}
