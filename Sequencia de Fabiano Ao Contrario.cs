107dea4a69691e4af57e382a22f1a35f6908e848
DanielNeres <danielnerres@gmail.com>
2023-12-10 19:24:26 -0300

    Sequencia de Fabiano Ao Contrario V.3.1

--- Sequencia de Fabiano Ao Contrario.cs
+++ Sequencia de Fabiano Ao Contrario.cs
@@ -1,17 +1,21 @@
-	using System;
+using System;
 
-    namespace sequenciadefabiano{
-    
-    class program{
+namespace sequenciadefabiano
+{
 
-        public static void main(){
-    int numeroimprecao, i; 
-// incia um for que percorre todas a sequencia de fibonacci, comeÃ§ando de i atÃ© i=0
-	for (i = 10; i >= 0; i--) {
-	// para calcular a sequencia de Fibonacci uso a formula: F(i) = (pow((1 + sqrt(5)), i) - pow((1 - sqrt(5)), i)) / (pow(2, i) * sqrt(5))
-		numeroimprecao = (pow((1 + sqrt(5)), i) - (pow((1 - sqrt(5)), i))) /(pow(2, i) * sqrt(5));
-        console.WriteLine(numeroimprecao);
+    class program
+    {
+
+        public static void main()
+        {
+            int numeroimprecao, i;
+            // incia um for que percorre todas a sequencia de fibonacci, comeÃ§ando de i atÃ© i=0
+            for (i = 10; i >= 0; i--)
+            {
+                // para calcular a sequencia de Fibonacci uso a formula: F(i) = (pow((1 + sqrt(5)), i) - pow((1 - sqrt(5)), i)) / (pow(2, i) * sqrt(5))
+                numeroimprecao = (pow((1 + sqrt(5)), i) - (pow((1 - sqrt(5)), i))) / (pow(2, i) * sqrt(5));
+                console.WriteLine(numeroimprecao);
+            }
+        }
     }
-    }
-    }
-    }
+}
