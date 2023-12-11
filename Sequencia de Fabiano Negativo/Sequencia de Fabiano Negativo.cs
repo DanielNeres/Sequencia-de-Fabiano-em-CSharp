e23251d1027e610453f219c59d17ab0ff2196d1c
DanielNeres <danielnerres@gmail.com>
2023-12-11 20:25:53 -0300

    Sequencia de Fabiano Negativo

--- Sequencia de Fabiano Negativo/Sequencia de Fabiano Negativo.cs
+++ Sequencia de Fabiano Negativo/Sequencia de Fabiano Negativo.cs
@@ -15,7 +15,7 @@
                 // atualiza o numero anterior que foi usado na impreção, por meio da diferença entre o numero impreço e o numero anterior ao anterior deste, por exemplo 8, e feita a diferença entre 8 e dois numeros anteriores a ele, assim ficaria 8 - 3 = 5 que é o numero anterior a 8
                 // vale salientar que essa variavel só começa a registrar o numero anterior a impreção ao final do primeiro laço, e que essa logica só começa a fazer sentido após o primeiro laço
                 numeroanterior = numeroimprecao - numeroanterior; 
-            }
+            } // rasaltando que a logica para imprimir começando com o numero 1, e a mesma, a unica mudança será o -1 no lugar do 1
         }
     }
 }
